ca300ae5b442af933b3849eb743b568c3522ea21
zeein <zeein81@gmail.com>
2022-08-21 14:04:25 +0900

    swiper 추가

--- functions.php
+++ functions.php
@@ -1,6 +1,10 @@
 <?php
 function wpdocs_theme_name_scripts() {
   wp_enqueue_style( 'ez-swiper', 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css', array(), 'v1.0' );
-  wp_enqueue_script( 'ez-script', 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js', array(), '1.0.0', true );
+  wp_enqueue_script( 'ez-swiper', 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js', array(), 'v1.0.0', true );
+
+  // script.js
+  wp_enqueue_script( 'ez-script', get_stylesheet_directory_uri(). '/script.js' , array(), 'v1.0.0', true );
+
 }
 add_action( 'wp_enqueue_scripts', 'wpdocs_theme_name_scripts' );
--- page-main.php
+++ page-main.php
@@ -3,7 +3,21 @@ get_header();
 ?>
 <main id="primary" class="site-main">
   <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    page-main.php
+    <section class="hero">
+      <!-- Slider main container -->
+      <div class="swiper">
+        <!-- Additional required wrapper -->
+        <div class="swiper-wrapper">
+          <!-- Slides -->
+          <div class="swiper-slide">Slide 1</div>
+          <div class="swiper-slide">Slide 2</div>
+          <div class="swiper-slide">Slide 3</div>
+          ...
+        </div>
+        <!-- If we need pagination -->
+        <div class="swiper-pagination"></div>
+      </div>
+    </section>
   </article>
 </main>
 <?php
